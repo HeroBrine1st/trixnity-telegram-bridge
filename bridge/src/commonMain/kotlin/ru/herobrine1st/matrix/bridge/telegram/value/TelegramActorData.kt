eb5026300681fb4e5701d30f43352048b75e4490
HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>
2025-04-19 19:57:45 +0300

    Add actor admin feature

--- bridge/src/commonMain/kotlin/ru/herobrine1st/matrix/bridge/telegram/value/TelegramActorData.kt
+++ bridge/src/commonMain/kotlin/ru/herobrine1st/matrix/bridge/telegram/value/TelegramActorData.kt
@@ -1,8 +1,10 @@
 package ru.herobrine1st.matrix.bridge.telegram.value
 
 import kotlinx.serialization.Serializable
+import net.folivo.trixnity.core.model.UserId
 
 @Serializable
 data class TelegramActorData(
-    val token: String
+    val token: String,
+    val admin: UserId = UserId("@empty:for.migration"),
 )
