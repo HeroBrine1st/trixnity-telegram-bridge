a85252109e52fe0ad88716e207449cfe8382071a
HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>
2025-05-05 17:53:16 +0300

    Add spotless plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,26 @@ plugins {
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.shadow) apply false
+    alias(libs.plugins.spotless)
 }
 
 allprojects {
     group = "ru.herobrine1st.matrix.bridge.telegram"
     version = "1.0.0-SNAPSHOT"
+
+    apply(plugin = "com.diffplug.spotless")
+    // Use predeclareDeps once this is huge
+    // https://github.com/diffplug/spotless/tree/main/plugin-gradle#dependency-resolution-modes
+    if (project != rootProject) spotless {
+        isEnforceCheck = false
+        kotlin {
+            target("src/*/kotlin/**/*.kt")
+            ktlint()
+                .setEditorConfigPath("$rootDir/.editorconfig")
+                .editorConfigOverride(mapOf(
+                    "ktlint_code_style" to "intellij_idea",
+                    "ktlint_standard_function-expression-body" to "disabled",
+                ))
+        }
+    }
 }
